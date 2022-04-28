c0fd41f489b092a01024a622483ade7e98eb8668
ICARUS-2 <60612247+ICARUS-2@users.noreply.github.com>
2022-04-28 10:08:30 -0400

    Added donate page

--- ElliptiKeys/src/app/donate/donate.component.ts
+++ ElliptiKeys/src/app/donate/donate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-donate',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titleService: Title) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle("Donate | ElliptiKeys")
   }
 
 }
