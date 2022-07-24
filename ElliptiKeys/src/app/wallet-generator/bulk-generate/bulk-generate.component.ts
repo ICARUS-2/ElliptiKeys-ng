03fc3d942e3e8e942a39d35e54f38f2ae5eb1f62
ICARUS-2 <ebriffett01@gmail.com>
2022-07-24 19:57:28 -0400

    Added default value for quantity on bulk generate

--- ElliptiKeys/src/app/wallet-generator/bulk-generate/bulk-generate.component.ts
+++ ElliptiKeys/src/app/wallet-generator/bulk-generate/bulk-generate.component.ts
@@ -1,7 +1,10 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ADDRESS_TYPES } from 'lib/address-types';
+import { NETWORK_TYPES } from 'lib/network-types';
 import { AddressTypeSelectorComponent } from '../address-type-selector/address-type-selector.component';
 import { NetworkTypeSelectorComponent } from '../network-type-selector/network-type-selector.component';
+import SingleAddressModel from './../../../../models/single-address-model';
 
 @Component({
   selector: 'app-bulk-generate',
@@ -10,6 +13,8 @@ import { NetworkTypeSelectorComponent } from '../network-type-selector/network-t
 })
 export class BulkGenerateComponent implements OnInit {
 
+  static MAX_GEN = 20000;
+
   addressTypes = ADDRESS_TYPES;
 
   networkType: string = NetworkTypeSelectorComponent.DEFAULT_VALUE;
@@ -17,7 +22,11 @@ export class BulkGenerateComponent implements OnInit {
 
   isCurrentlyGenerating: boolean = false;
 
-  qtyToGenerate: number = 0;
+  qtyToGenerate: number = 10;
+
+  displayedKeys: SingleAddressModel[] = [];
+
+  countErrorMessage: string = "";
 
   constructor() { }
 
@@ -34,9 +43,28 @@ export class BulkGenerateComponent implements OnInit {
     this.selectedAddressType = value;
   }
 
-  bulkGenerate()
+  async bulkGenerate()
   {
+    if (this.qtyToGenerate == null || this.qtyToGenerate > BulkGenerateComponent.MAX_GEN || this.qtyToGenerate <=0)
+    {
+      this.countErrorMessage = "Quantity must be a number between 0 and "+ BulkGenerateComponent.MAX_GEN;
+      return;
+    }
+    
+    let isTestnet: boolean = this.networkType == NETWORK_TYPES.testnet;
 
+    let tempArr: SingleAddressModel[] = []
+
+    this.isCurrentlyGenerating = true;
+
+    for(let i = 0; i < this.qtyToGenerate; i++)
+    {
+      tempArr.push( SingleAddressModel.create(isTestnet, this.selectedAddressType) )
+    }
+
+    this.isCurrentlyGenerating = false;
+
+    this.displayedKeys = tempArr;
   }
 
 }
