3efe69bb11a9069ca8c677fc473db36b6720a358
ICARUS-2 <60612247+ICARUS-2@users.noreply.github.com>
2023-05-22 15:28:34 -0400

    Implemented auto-gen incrementing, fixed stop setting button visual glitch

--- ElliptiKeys/src/app/services/auto-gen/auto-gen.service.ts
+++ ElliptiKeys/src/app/services/auto-gen/auto-gen.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import PageHelper from './../../../../lib/page-helper';
 import LocalStorageHelper from 'lib/localstorage-helper';
+import { AUTO_GEN_PAGE_SELECTION_TYPES } from 'lib/page-selection-types';
 
 @Injectable({
   providedIn: 'root'
@@ -20,20 +21,39 @@ export class AutoGenService {
   navigateAfterDelay(isTestnet: boolean, pageNumber: BigInt)
   {
     let interval = PageHelper.DELAY;
+    let networkUrl = isTestnet ? "testnet" : "bitcoin";
 
     this.timeoutEvent = setTimeout( () => 
     {
       if(!this.autoModeActive)
         return;
-    
-      
-      if(isTestnet)
+
+      let autoGenType = LocalStorageHelper.GetAutoGenPageSelectionType();
+
+      if (autoGenType == AUTO_GEN_PAGE_SELECTION_TYPES.incrementing)
       {
-        this.router.navigate(['/testnet-random'])
+        console.log(pageNumber)
+
+        //@ts-ignore
+        let nextPage = pageNumber == PageHelper.GetMaxPage() ? BigInt('1') : pageNumber + BigInt('1');
+        
+        this.router.navigate(["/" + networkUrl + "/" + nextPage]);
+      }
+      else if (autoGenType == AUTO_GEN_PAGE_SELECTION_TYPES.decrementing)
+      {
+
       }
       else
       {
-        this.router.navigate(['/random'])
+        //If the user didn't set auto-gen to increment/decrement, it must be random
+        if(isTestnet)
+        {
+          this.router.navigate(['/testnet-random'])
+        }
+        else
+        {
+          this.router.navigate(['/random'])
+        }
       }
       
 
