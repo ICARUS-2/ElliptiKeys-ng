7d75929039128399b76a16bac632665423a46f1b
ICARUS-2 <60612247+ICARUS-2@users.noreply.github.com>
2023-05-30 14:42:03 -0400

    Key status display setting works

--- ElliptiKeys/src/app/current-page-stats-display/current-page-stats-display.component.ts
+++ ElliptiKeys/src/app/current-page-stats-display/current-page-stats-display.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { KEY_STATUS_DISPLAY_TYPES } from 'lib/dictionaries/key-status-display-types';
+import LocalStorageHelper from 'lib/localstorage-helper';
 
 @Component({
   selector: 'app-current-page-stats-display',
@@ -10,10 +12,17 @@ export class CurrentPageStatsDisplayComponent implements OnInit {
   @Input() redCounter: number = 0;
   @Input() yellowCounter: number = 0;
   @Input() greenCounter: number = 0;
+
+  displayTypes = KEY_STATUS_DISPLAY_TYPES;
   
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getCurrentDisplayType() : string
+  {
+    return LocalStorageHelper.GetKeyStatusDisplayType();
   }
 
 }
